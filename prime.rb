d6a049b28a9c513b63cf7651b0b8dfce28578665
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 17:53:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
+require 'pry'
 # Add  code here!
+def prime?(int)
+  isPrime = false
+  if int <= 1
+    isPrime = false
+  elsif int == 2 || 3
+    isPrime = true
+  else
+    range = 2..int
+
+    for prime_test in range do
+      check1 = int % prime_test
+      if check1 < 1
+        binding.pry
+        puts "The number is not prime."
+        isPrime = false
+      end
+    end
+  end
+  isPrime
+end
