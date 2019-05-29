a26808c4b016b872a01ef723703c75108f5f6d6b
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-29 17:56:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,13 +2,14 @@ require_relative 'pry'
 # Add  code here!
 def prime?(int)
   isPrime = false
+  binding.pry
   if int <= 1
     isPrime = false
   elsif int == 2 || 3
     isPrime = true
   else
     range = 2..int
-        binding.pry
+
     for prime_test in range do
       check1 = int % prime_test
       if check1 < 1
